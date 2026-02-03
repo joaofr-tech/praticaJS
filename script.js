265e895b701de6862f5bbf0489311f1383cb0e28
João Francisco <JF756061@gmail.com>
2026-02-03 16:20:17 -0300

    contador c/ botao +5 e -5

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const display = document.querySelector('#display')
 const add = document.querySelector('#adicionar1')
 const sub = document.querySelector('#subtrair1')
+const add5 = document.querySelector('#adicionar5')
+const sub5 = document.querySelector('#subtrair5')
 
 let n = 0
 
 add.addEventListener('click', adicionar)
 
+add5.addEventListener('click', adicionar5)
+
 sub.addEventListener('click', subtrair)
+
+sub5.addEventListener('click', subtrair5)
 
 function adicionar(){
     n++
     display.innerHTML = '<p>'+n+'</p>'
 }
 
+function adicionar5(){
+    n += 5
+    display.innerHTML = '<p>'+n+'</p>'
+}
+
 function subtrair(){
     n--
+    display.innerHTML = '<p>'+n+'</p>'
+}
+
+function subtrair5(){
+    n -= 5
     display.innerHTML = '<p>'+n+'</p>'
 }
